73909b025aedd62bcc627b4977e6cf1416dfcd6c
SadeemQayyum <66385@students.riphah.edu.pk>
2025-12-06 08:15:58 -0800

    Update Q5.asm

--- Task 8/Q5.asm
+++ Task 8/Q5.asm
@@ -1,31 +1,26 @@
 org 100h
 .data
-a db "SADEEM$"; Name
+a db "SADEEM$"
 .code
 
-mov ax, @data
+mov ax,@data
 mov ds,ax
-mov si,offset a ;SI points to the start of the string 'S'
-mov cx,6; CX is the loop counter (6 characters: S, A, D, E, E, M)
 
+mov si,offset a
+mov cx,6
+
+; Push characters into stack
 L1:
-mov dl, [si]
+mov dl,[si]
+push dx
 inc si
-mov ah, 2
-int 21h   
 loop L1
 
+mov cx,6
+
+; Pop characters and display in reverse
 L2:
 pop dx
 mov ah,2
-mov dl,dl
 int 21h
-Loop L2
-
-
-
-
-
-
-
-
+loop L2
